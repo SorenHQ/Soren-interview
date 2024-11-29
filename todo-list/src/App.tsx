86b7c27ec5b365df7efc41e973e116652a768aff
nima khaki <nimakhaki123@gmail.com>
2024-11-29 12:07:14 +0330

    main tasks completed

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,11 +1,12 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { sortArrayByBoolean } from "./utils/sort";
 import { v4 as uuid } from "uuid";
 import styled from "@emotion/styled";
 import { AddInput } from "./components/AddInput";
 import { TodoItem } from "./components/TodoItem";
 import { TodoList } from "./components/TodoList";
 import { Header } from "./components/Header";
-
+import { setStore, readStore } from "./utils/store";
 const Wrapper = styled.div({
   display: "flex",
   flexDirection: "column",
@@ -22,48 +23,75 @@ const Wrapper = styled.div({
  */
 
 function App() {
-  const [initialData, setTodo] = useState<Todo[]>([
-    {
-      id: uuid(),
-      label: "Buy groceries",
-      checked: false,
-    },
-    {
-      id: uuid(),
-      label: "Reboot computer",
-      checked: false,
-    },
-    {
-      id: uuid(),
-      label: "Ace CoderPad interview",
-      checked: true,
-    },
-  ]);
-  const [todos, setTodos] = useState<Todo[]>(initialData);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodo = useCallback((label: string) => {
-    setTodos((prev) => [
-      {
-        id: uuid(),
-        label,
-        checked: false,
-      },
-      ...prev,
-    ]);
+    setTodos((prev) => {
+      let newTodo = [
+        {
+          id: uuid(),
+          label,
+          checked: false,
+        },
+        ...prev,
+      ];
+      setStore(newTodo, "todos");
+      return newTodo;
+    });
   }, []);
 
-  const handleChange = useCallback((checked: boolean, id: string) => {
-    // handle the check/uncheck logic
-    const nextCounters = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.checked = checked;
-        return todo;
-      } else {
-        // The rest haven't changed
-        return todo;
-      }
-    });
-    setTodo(nextCounters);
+  const handleChange = useCallback(
+    (checked: boolean, id: string) => {
+      // handle the check/uncheck logic
+      const modifiedTodos = todos.filter((todo) => {
+        if (todo.id === id) {
+          todo.checked = checked;
+          return todo;
+        } else {
+          // The rest haven't changed
+          return todo;
+        }
+      });
+      const sorted = sortTodos(modifiedTodos);
+      setTodos(sorted);
+      setStore(sorted, "todos");
+    },
+    [todos]
+  );
+  const sortTodos = (unsorted: Todo[]): Todo[] => {
+    const sorted = sortArrayByBoolean<Todo>(unsorted, "checked", true);
+    return sorted;
+  };
+  const populatePage = () => {
+    const initData: Todo[] = [
+      {
+        id: uuid(),
+        label: "Buy groceries",
+        checked: false,
+      },
+      {
+        id: uuid(),
+        label: "Reboot computer",
+        checked: false,
+      },
+      {
+        id: uuid(),
+        label: "Ace CoderPad interview",
+        checked: true,
+      },
+    ];
+    const previousTodos = readStore<Todo>("todos");
+
+    if (previousTodos.length !== 0) {
+      const sorted = sortTodos(previousTodos);
+      setTodos(sorted);
+    } else {
+      setStore(initData, "todos");
+      setTodos(initData);
+    }
+  };
+  useEffect(() => {
+    populatePage();
   }, []);
 
   return (
